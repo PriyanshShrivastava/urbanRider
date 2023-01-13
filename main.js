2ddb581db3381bcabcdede48649827a509188976
PriyanshShrivastava <learn.priyansh@gmail.com>
2023-01-13 20:21:54 +0530

    docs: uploading code files first time

---
+++ main.js
@@ -0,0 +1,36 @@
+const mobileNav = document.querySelector(".btn-mobile-nav");
+const headerMain = document.querySelector("#header-section");
+
+mobileNav.addEventListener("click", function () {
+  headerMain.classList.toggle("nav-open");
+});
+
+const navLinks = document.querySelectorAll(".main-nav-link");
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    if (headerMain.classList.contains("nav-open")) {
+      headerMain.classList.remove("nav-open");
+    }
+  });
+});
+
+// Sticky menu
+const sectHero = document.querySelector("#section-hero");
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      headerMain.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      headerMain.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-64px",
+  }
+);
+observer.observe(sectHero);
